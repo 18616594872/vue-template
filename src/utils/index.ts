a8192b4007e2ef54c6183ef2f0f80b82e41ca3d8
henglei.dong <1960492759@qq.com>
2019-12-21 22:20:37 +0800

    添加mock数据

---
+++ src/utils/index.ts
@@ -0,0 +1,19 @@
+/**
+ * @param {string} url
+ * @returns {Object}
+ */
+export function param2Obj(url: string): object {
+    const search: string = url.split('?')[1]
+    if (!search) {
+        return {}
+    }
+    return JSON.parse(
+        '{"' +
+        decodeURIComponent(search)
+            .replace(/"/g, '\\"')
+            .replace(/&/g, '","')
+            .replace(/=/g, '":"')
+            .replace(/\+/g, ' ') +
+        '"}'
+    )
+}
---
+++ vue.config.js
@@ -0,0 +1,49 @@
+'use strict'
+const path = require('path')
+
+const resolve = dir => {
+    return path.join(__dirname, dir)
+}
+
+
+// If your port is set to 80,
+// use administrator privileges to execute the command line.
+// For example, Mac: sudo npm run
+// You can change the port by the following method:
+// port = 9527 npm run dev OR npm run dev --port = 9527
+const port = process.env.port || process.env.npm_config_port || 8080 // dev port
+
+module.exports = {
+    publicPath: '/',
+    outputDir: 'dist', // 打包生成的生产环境构建文件的目录
+    assetsDir: 'static', // 放置生成的静态资源路径，默认在outputDir
+    indexPath: 'index.html', // 指定生成的 index.html 输入路径，默认outputDir
+    pages: undefined, // 构建多页
+    productionSourceMap: false, // 开启 生产环境的 source map?
+    chainWebpack: config => {
+        // 配置路径别名
+        config.resolve.alias
+            .set('@', resolve('src'))
+            .set('_c', resolve('src/components'))
+    },
+    css: {
+        modules: false, // 启用 CSS modules
+        extract: true, // 是否使用css分离插件
+        sourceMap: false, // 开启 CSS source maps?
+        loaderOptions: {} // css预设器配置项
+    },
+    devServer: {
+        port: port, // 端口
+        proxy: {
+            // change xxx-api/login => mock/login
+            [process.env.VUE_APP_BASE_API]: {
+                target: `http://127.0.0.1:${port}/mock`,
+                changeOrigin: true,
+                pathRewrite: {
+                    ['^' + process.env.VUE_APP_BASE_API]: ''
+                }
+            }
+        },
+        after: require('./mock/mock-server.js')
+    }
+}
